fa2373ce577168d1ef9d52dfe5dda5b3c4ca9174
Lakkanna Walikar <lakkanna.walikar@hotelsoft.com>
2020-09-02 17:59:34 +0530

    read app.json file version

--- scripts/actions.js
+++ scripts/actions.js
@@ -1,1 +1,12 @@
 console.log('action called')
+const fs = require('fs');
+const path = require('path');
+
+fs.readFile(path.join('app.json'), 'utf-8', (error, dataString) => {
+  if (error) {
+    console.error(error);
+    throw new Error(error);
+  }
+  const data = JSON.parse(dataString);
+  console.log('VERSION: ', data.version)
+})
