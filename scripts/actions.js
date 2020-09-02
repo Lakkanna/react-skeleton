89e9bcb70c415e605e617319f98cc264d29ce815
Lakkanna Walikar <lakkanna.walikar@hotelsoft.com>
2020-09-02 18:20:33 +0530

    version bump improvise

--- scripts/actions.js
+++ scripts/actions.js
@@ -1,26 +1,24 @@
-console.log('action called')
 const fs = require('fs');
 const path = require('path');
 
 
-const bumpUpVersion = (versions, majorRelease = false) => {
-	let [major, minor, patch] = versions;
-	if (majorRelease) {
-		return [parseInt(major) + 1, 0, 0];
+const bumpUpVersion = (versions) => {
+  let [major, minor, patch] = versions.map(value => parseInt(value));
+	if (minor === 9 && patch === 9) {
+		return [ major + 1, 0, 0];
 	} else {
-		[minor, patch] = parseInt(patch) < 9 ? [minor, parseInt(patch) + 1] : [parseInt(minor) + 1, 0];
+		[minor, patch] = patch < 9 ? [minor, patch + 1] : [minor + 1, 0];
 		return [major, minor, patch];
 	}
 };
 
 fs.readFile(path.join('app.json'), 'utf-8', (error, dataString) => {
   if (error) {
-    console.error(error);
     throw new Error(error);
   }
   const data = JSON.parse(dataString);
   console.log('VERSION: ', data.version);
   const versions = data.version.split('.');
-  const newVersions = bumpUpVersion(versions, false);
+  const newVersions = bumpUpVersion(versions);
   console.log('NEW VERSION: ', newVersions.join('.'));
 })
