aed278b1322673c0af05de4a0244b60a62af1660
Lakkanna Walikar <lakkanna.walikar@hotelsoft.com>
2020-09-02 18:04:35 +0530

    version bump up

--- scripts/actions.js
+++ scripts/actions.js
@@ -2,11 +2,25 @@ console.log('action called')
 const fs = require('fs');
 const path = require('path');
 
+
+const bumpUpVersion = (versions, majorRelease = false) => {
+	let [major, minor, patch] = versions;
+	if (majorRelease) {
+		return [_.toNumber(major) + 1, 0, 0];
+	} else {
+		[minor, patch] = _.toNumber(patch) < 9 ? [minor, _.toNumber(patch) + 1] : [_.toNumber(minor) + 1, 0];
+		return [major, minor, patch];
+	}
+};
+
 fs.readFile(path.join('app.json'), 'utf-8', (error, dataString) => {
   if (error) {
     console.error(error);
     throw new Error(error);
   }
   const data = JSON.parse(dataString);
-  console.log('VERSION: ', data.version)
+  console.log('VERSION: ', data.version);
+  const versions = data.version.split('.');
+  const newVersios = bumpUpVersion(versions, false);
+  console.log('NEW VERSION: ', newVersions.joint('.'));
 })
